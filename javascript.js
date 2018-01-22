52e3eb2118cfdf746a6add31c5aeed2b3752956f
Eliram <Eliram@MacBook-Pro-3.local>
2018-01-22 00:26:34 -0500

    first

---
+++ javascript.js
@@ -0,0 +1,10 @@
+//JavaScript
+var urrency = "ripple"
+var queryURL = "https://poloniex.com/api"+"PDZKJRU2-1LL9KSLE-YU57IHR6-YKZ56UOL";
+
+$.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+    	
+    }
